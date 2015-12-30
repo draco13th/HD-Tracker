8830c49f590d2621dcebb25195494d5b021e2433
draco13th <fgvedxfedtfc@hotmail.com>
2015-12-29 21:21:07 -0600

    Implemented geolocation

--- js.js
+++ js.js
@@ -1,9 +1,22 @@
 //Variables
 var chrome_points = 0;
+var test_loc = document.getElementById("demo");
 
 //Functions
 function test_chromepoints() {
-	chrome_points = chrome_points + 1
+	chrome_points = chrome_points + 1;
+}
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		test_loc.innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+
+function showPosition(position) {
+	test_loc.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 }
 
 //HTML Updates
