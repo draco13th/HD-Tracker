690f4a6a34a7be77668bcea3d4f4e15c93505846
draco13th <fgvedxfedtfc@hotmail.com>
2015-12-30 18:47:25 -0600

    Mapping
    Testing mapping using Google Maps api, and current location data.  Found
    that chrome issue is only browser based, doesn't effect Android app.

--- js.js
+++ js.js
@@ -9,15 +9,48 @@ function test_chromepoints() {
 //Snag the user's location data
 function getLocation() {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition);
+		navigator.geolocation.getCurrentPosition(showPosition, showError);
 	} else {
-		document.getElementById("location_data").innerHTML = "Geolocation not supported in this browser :("
+		document.getElementById("error").innerHTML = "Geolocation not supported in this browser :("
 	}
 }
 
 //Display the user's location data
 function showPosition(position) {
-	document.getElementById("location_data").innerHTML = "Latitude: " + position.coords.latitude + " | " + "Longitude: " + position.coords.longitude;
+	lat = position.coords.latitude;
+	lon = position.coords.longitude;
+	latlon = new google.maps.LatLng(lat, lon)
+	mapholder = document.getElementById('mapholder')
+	mapholder.style.height = "250px";
+	mapholder.style.width = "500px";
+	
+	var myOptions = {
+	center:latlon,zoom:14,
+	mapTypeId:google.maps.MapTypeId.ROADMAP,
+	mapTypeControl:false,
+	navigationControlOptions:{style:google.maps.NavigationControLStyle.SMALL}
+	}
+	
+	var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+	var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+}
+
+//If there's an error, explain it
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("error").innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("error").innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            document.getElementById("error").innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("error").innerHTML = "An unknown error occurred."
+            break;
+    }
 }
 
 //HTML Updates
