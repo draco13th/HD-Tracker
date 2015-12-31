2ae1567df2323f91eb511af958b48b202048e6a2
draco13th <fgvedxfedtfc@hotmail.com>
2015-12-30 19:11:52 -0600

    Testing google maps API

--- js.js
+++ js.js
@@ -6,6 +6,28 @@ function test_chromepoints() {
 	chrome_points = chrome_points + 1;
 }
 
+//Get current position
+function getLocation() {
+	navigator.geolocation.getCurrentPosition(Initialize);
+}
+//Initialize Mapping
+function Initialize(position) {
+	//Set the canvas as the Div element
+	var mapCanvas = document.getElementById('map');
+	//Setup the latitude/longitude pair as the current positon
+	lat = position.coords.latitude;
+	lon = position.coords.longitude;
+	latlon = new google.maps.LatLng(lat, lon);
+	//Set map options
+	var mapOptions = {
+		center: latlon,
+		zoom: 8,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	//Loads the map options into the map
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
 //HTML Updates
 window.setInterval(function(){
 	
